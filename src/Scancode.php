066ffcde162719ad37f5cb87855c833711ee9258
Iskander Sharipov <i.sharipov@corp.vk.com>
2021-09-29 17:06:00 +0300

    add missing scancodes

--- src/Scancode.php
+++ src/Scancode.php
@@ -3,8 +3,195 @@
 namespace Quasilyte\SDLite;
 
 class Scancode {
+    const UNKNOWN = 0;
+
+    const A = 4;
+    const B = 5;
+    const C = 6;
+    const D = 7;
+    const E = 8;
+    const F = 9;
+    const G = 10;
+    const H = 11;
+    const I = 12;
+    const J = 13;
+    const K = 14;
+    const L = 15;
+    const M = 16;
+    const N = 17;
+    const O = 18;
+    const P = 19;
+    const Q = 20;
+    const R = 21;
+    const S = 22;
+    const T = 23;
+    const U = 24;
+    const V = 25;
+    const W = 26;
+    const X = 27;
+    const Y = 28;
+    const Z = 29;
+
+    const NUM1 = 30;
+    const NUM2 = 31;
+    const NUM3 = 32;
+    const NUM4 = 33;
+    const NUM5 = 34;
+    const NUM6 = 35;
+    const NUM7 = 36;
+    const NUM8 = 37;
+    const NUM9 = 38;
+    const NUM0 = 39;
+
+    const RETURN = 40;
+    const ESCAPE = 41;
+    const BACKSPACE = 42;
+    const TAB = 43;
+    const SPACE = 44;
+
+    const MINUS = 45;
+    const EQUALS = 46;
+    const LEFTBRACKET = 47;
+    const RIGHTBRACKET = 48;
+    const BACKSLASH = 49;
+
+    const NONUSHASH = 50;
+
+    const SEMICOLON = 51;
+    const APOSTROPHE = 52;
+    const GRAVE = 53;
+
+    const COMMA = 54;
+    const PERIOD = 55;
+    const SLASH = 56;
+
+    const CAPSLOCK = 57;
+
+    const F1 = 58;
+    const F2 = 59;
+    const F3 = 60;
+    const F4 = 61;
+    const F5 = 62;
+    const F6 = 63;
+    const F7 = 64;
+    const F8 = 65;
+    const F9 = 66;
+    const F10 = 67;
+    const F11 = 68;
+    const F12 = 69;
+
+    const PRINTSCREEN = 70;
+    const SCROLLLOCK = 71;
+    const PAUSE = 72;
+    const INSERT = 73;
+
+    const HOME = 74;
+    const PAGEUP = 75;
+    const DELETE = 76;
+    const END = 77;
+    const PAGEDOWN = 78;
+
     const RIGHT = 79;
     const LEFT = 80;
     const DOWN = 81;
     const UP = 82;
+
+    const NUMLOCKCLEAR = 83;
+
+    const KP_DIVIDE = 84;
+    const KP_MULTIPLY = 85;
+    const KP_MINUS = 86;
+    const KP_PLUS = 87;
+    const KP_ENTER = 88;
+    const KP_1 = 89;
+    const KP_2 = 90;
+    const KP_3 = 91;
+    const KP_4 = 92;
+    const KP_5 = 93;
+    const KP_6 = 94;
+    const KP_7 = 95;
+    const KP_8 = 96;
+    const KP_9 = 97;
+    const KP_0 = 98;
+    const KP_PERIOD = 99;
+
+    const NONUSBACKSLASH = 100;
+    const APPLICATION = 101;
+    const POWER = 102;
+
+    const KP_EQUALS = 103;
+    const F13 = 104;
+    const F14 = 105;
+    const F15 = 106;
+    const F16 = 107;
+    const F17 = 108;
+    const F18 = 109;
+    const F19 = 110;
+    const F20 = 111;
+    const F21 = 112;
+    const F22 = 113;
+    const F23 = 114;
+    const F24 = 115;
+    const EXECUTE = 116;
+    const HELP = 117;
+    const MENU = 118;
+    const SELECT = 119;
+    const STOP = 120;
+    const AGAIN = 121;
+    const UNDO = 122;
+    const CUT = 123;
+    const COPY = 124;
+    const PASTE = 125;
+    const FIND = 126;
+    const MUTE = 127;
+    const VOLUMEUP = 128;
+    const VOLUMEDOWN = 129;
+
+    const KP_COMMA = 133;
+    const KP_EQUALSAS400 = 134;
+
+    const LCTRL = 224;
+    const LSHIFT = 225;
+    const LALT = 226;
+    const LGUI = 227;
+    const RCTRL = 228;
+    const RSHIFT = 229;
+    const RALT = 230;
+    const RGUI = 231;
+
+    const MODE = 257;
+
+    const AUDIONEXT = 258;
+    const AUDIOPREV = 259;
+    const AUDIOSTOP = 260;
+    const AUDIOPLAY = 261;
+    const AUDIOMUTE = 262;
+    const MEDIASELECT = 263;
+    const WWW = 264;
+    const MAIL = 265;
+    const CALCULATOR = 266;
+    const COMPUTER = 267;
+    const AC_SEARCH = 268;
+    const AC_HOME = 269;
+    const AC_BACK = 270;
+    const AC_FORWARD = 271;
+    const AC_STOP = 272;
+    const AC_REFRESH = 273;
+    const AC_BOOKMARKS = 274;
+
+    const BRIGHTNESSDOWN = 275;
+    const BRIGHTNESSUP = 276;
+    const DISPLAYSWITCH = 277;
+
+    const KBDILLUMTOGGLE = 278;
+    const KBDILLUMDOWN = 279;
+    const KBDILLUMUP = 280;
+    const EJECT = 281;
+    const SLEEP = 282;
+
+    const APP1 = 283;
+    const APP2 = 284;
+
+    const AUDIOREWIND = 285;
+    const AUDIOFASTFORWARD = 286;
 }
